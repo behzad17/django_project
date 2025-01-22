08c3f5a4deb99d14e98428969f759b35e5b6580b
Behzad Javadian <behzad_1349@hotmail.com>
2025-01-22 17:35:19 +0000

    rewiew some files

--- my_project/urls.py
+++ my_project/urls.py
@@ -20,8 +20,6 @@ from django.urls import path, include
 from hello_world import views as index_views
 from about import views as about_views
 
-
-
 urlpatterns = [
     path('hello/', index_views.index, name='index'),
     path('about/', about_views.about_me, name='about'),
